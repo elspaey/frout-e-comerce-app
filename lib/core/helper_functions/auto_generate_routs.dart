d42de9dd4716a941e5cdff0b75c6d5dba3da5a8c
elspaey <m.elspaey3@gmail.com>
2024-12-14 00:26:03 +0200

    splash view logic done

--- lib/core/helper_functions/auto_generate_routs.dart
+++ lib/core/helper_functions/auto_generate_routs.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:frout_ecomerce_app/features/onBoarding/presentation/views/on_boarding_view.dart';
 import 'package:frout_ecomerce_app/features/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> ongenerateRout(RouteSettings setting) {
   switch (setting.name) {
     case Splashview.routeName:
       return MaterialPageRoute(builder: (context) => Splashview());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => OnBoardingView());
     default:
       return MaterialPageRoute(builder: (context) => Scaffold());
   }
--- lib/features/splash/presentation/views/splash_widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/splash_widgets/splash_view_body.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:frout_ecomerce_app/core/utils/assets_imeges_class.dart';
+import 'package:frout_ecomerce_app/features/onBoarding/presentation/views/on_boarding_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    excuteNavigation();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Row(
@@ -17,8 +30,21 @@ class SplashViewBody extends StatelessWidget {
           ],
         ),
         SvgPicture.asset(Assets.imagesLogo),
-        SvgPicture.asset(Assets.imagesCircles,fit: BoxFit.fill,),
+        SvgPicture.asset(
+          Assets.imagesCircles,
+          fit: BoxFit.fill,
+        ),
       ],
     );
   }
+
+  void excuteNavigation() {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
+  }
+
+  // void excuteNavigation() {}
 }
+
+// void excuteNavigation() {}
